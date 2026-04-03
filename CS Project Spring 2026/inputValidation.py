7383db3d269fb0cad6cfa009e7a398cd5749963c
Zachary Zdenovec <zacharyzdenovec@gmail.com>
2026-04-03 12:06:47 -0500

    Removed redundancy of code in inputValidation.py

--- CS Project Spring 2026/inputValidation.py
+++ CS Project Spring 2026/inputValidation.py
@@ -4,20 +4,3 @@ check = inputValidation(password)
     while check:
         password = input("Invalid Password. Try another one: ")
         check = inputValidation(password)
-
-# INPUT VALIDATION
-# ----------------
-
-def inputValidation(password):
-    # make sure there is no space at the front,
-    # the back or in the middle of the string
-    if " " in password:
-        return True
-    
-    # make sure the string has at least 1 character 
-    # and at most 16 characters
-    elif len(password) < 1 or len(password) > 16:
-        return True
-    
-    else:
-        return False
